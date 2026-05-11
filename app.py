bed6e1205dfd1c4e30dae22586bc94f9260a8d05
SaiVardhan8 <msaivardhan8@gmail.com>
2026-05-11 12:04:07 +0530

    Updated application message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello DevOps World"
+    return "CI/CD Pipeline Working Successfully"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
